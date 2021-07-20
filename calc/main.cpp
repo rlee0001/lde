a66726ddd414543b0a4936f8a0205c5fee5a01af
Robert Lee <robert@bestow.com>
2021-07-20 01:58:22 -0500

    Try out cairo.

--- calc/main.cpp
+++ calc/main.cpp
@@ -1,33 +1,65 @@
-#include <X11/Intrinsic.h>
-#include <X11/StringDefs.h>
-#include <X11/Xaw/Label.h>
+#include<X11/Xlib.h>
+#include<X11/X.h>
+#include<X11/Xutil.h>
 
+#include<cairo/cairo.h>
+#include<cairo/cairo-xlib.h>
+
+#include<chrono>
 #include<iostream>
+#include<thread>
 
 using namespace std;
 
-void onClick(Widget widget, XtPointer client_data, XtPointer call_data)
+void draw(cairo_t *cr)
 {
-    cout << "Hello Yourself!" << endl;
+    cairo_set_source_rgba(cr, 1.0, 0.0, 0.0, 0.5);
+    cairo_rectangle(cr, 0, 0, 200, 200);
+    cairo_fill(cr);
 }
 
 int main(int argc, char* argv[])
 {
-    XtAppContext app_context;
-    Widget toplevel, hello;
+    Display *d = XOpenDisplay(NULL);
+    Window root = DefaultRootWindow(d);
+    int default_screen = XDefaultScreen(d);
 
-    toplevel = XtVaAppInitialize(
-        &app_context,
-        "XHello",
-        NULL, 0,
-        &argc, argv,
-        NULL,
-        NULL);
+    XSetWindowAttributes attrs;
+    attrs.override_redirect = true;
 
-    hello = XtVaCreateManagedWidget("hello", labelWidgetClass, toplevel, NULL);
+    XVisualInfo vinfo;
+    if (!XMatchVisualInfo(d, DefaultScreen(d), 32, TrueColor, &vinfo))
+    {
+        cout << "No visual found supporting 32-bit color." << endl;
+        return 1;
+    }
 
-    XtAddCallback(hello, XtNcallback, onClick, NULL);
+    attrs.colormap = XCreateColormap(d, root, vinfo.visual, AllocNone);
+    attrs.background_pixel = 0;
+    attrs.border_pixel = 0;
 
-    XtRealizeWidget(toplevel);
-    XtAppMainLoop(app_context);
+    Window overlay = XCreateWindow(
+        d, root,
+        0, 0, 200, 200, 0,
+        vinfo.depth, InputOutput,
+        vinfo.visual,
+        CWOverrideRedirect | CWColormap | CWBackPixel | CWBorderPixel, &attrs
+    );
+
+    XMapWindow(d, overlay);
+
+    cairo_surface_t *surf = cairo_xlib_surface_create(d, overlay, vinfo.visual, 200, 200);
+    cairo_t *cr = cairo_create(surf);
+
+    draw(cr);
+    XFlush(d);
+
+    this_thread::sleep_for(chrono::milliseconds(10000));
+
+    cairo_destroy(cr);
+    cairo_surface_destroy(surf);
+
+    XUnmapWindow(d, overlay);
+    XCloseDisplay(d);
+    return 0;
 }
